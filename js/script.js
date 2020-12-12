12694aac997fd880c958edfc5f7f7082a419e016
Adam Kosmala <kosmala.adam@yahoo.com>
2020-12-12 12:01:55 +0100

    index.html

--- js/script.js
+++ js/script.js
@@ -4,14 +4,31 @@
   });*/
 
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
   
     /* remove class 'active' from all article links  */
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+}
   
     /* add class 'active' to the clicked link */
+
+    for(let activeLink of activeLinks)
+    activeLink.classList.add('active');
+    console.log('clickedElement:', clickedElement);
   
     /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.post a.active');
+
+    for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+    }
   
     /* get 'href' attribute from the clicked link */
   
