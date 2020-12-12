8c66ab414939c515a4d0a361eac807f4a332d99e
Adam Kosmala <kosmala.adam@yahoo.com>
2020-12-12 18:11:45 +0100

    js/script.js

--- js/script.js
+++ js/script.js
@@ -51,7 +51,10 @@
     console.log(targetArticle);
 
     /* add class 'active' to the correct article */
-  }
+
+    targetArticle.classList.add('active');
+
+}
   
   const links = document.querySelectorAll('.titles a');
   
