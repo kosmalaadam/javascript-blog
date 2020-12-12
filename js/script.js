3cebcc578fade0cd865588b4dc3513ac158863d4
Adam Kosmala <kosmala.adam@yahoo.com>
2020-12-12 18:04:05 +0100

    add index.html

--- js/script.js
+++ js/script.js
@@ -4,13 +4,17 @@
   });*/
 
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
+
+   
+    
   
     /* remove class 'active' from all article links  */
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
+    const activeLinks = document.querySelectorAll('.titles a.active')
 
     for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
@@ -18,22 +22,34 @@
   
     /* add class 'active' to the clicked link */
 
-    for(let activeLink of activeLinks)
-    activeLink.classList.add('active');
+    
+    clickedElement.classList.add('active');
+    
     console.log('clickedElement:', clickedElement);
-  
+
     /* remove class 'active' from all articles */
 
-    const activeArticles = document.querySelectorAll('.post a.active');
+    const activeArticles = document.querySelectorAll('.posts post.active')
 
     for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
     }
   
     /* get 'href' attribute from the clicked link */
+
+    const articleSelector = document.getElementById('a');
+
+    activeLink = clickedElement.getAttribute('href');
+       
+    
+    console.log(articleSelector);
   
     /* find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector('.post');
   
+    console.log(targetArticle);
+
     /* add class 'active' to the correct article */
   }
   
